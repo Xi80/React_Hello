fe16efa605ed1e47c84d8292b1a472723965b27d
Luna <h31e26@ed.cc.suzuka-ct.ac.jp>
2021-09-11 11:32:22 +0900

    名前表示ができるように

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+//import App from "./App";
 
+
+interface Name {
+    firstName: string;
+    lastName: string;
+}
+
+function formatName(user : Name) : string{
+    return user.firstName + " " + user.lastName;
+}
+
+const user : Name = {
+    firstName : "Luna",
+    lastName : "Tsukinashi"
+};
+
+const elm = (
+    <h1>Hello,{formatName(user)}!</h1>
+);
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    elm,
     document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
